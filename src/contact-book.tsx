253a1186f71625eb3edd2246970d65289f72b063
Else Meijerink <61120785+SipofTea@users.noreply.github.com>
2025-02-24 17:27:58 +0000

    basic add and display of name and city without storage

--- src/contact-book.tsx
+++ src/contact-book.tsx
@@ -1,5 +1,49 @@
+import { BaseSyntheticEvent, useState } from "react";
+
 const ContactBook = () => {
-  return <div>Add your solution here</div>;
+  const [contacts, setContacts] = useState([]);
+
+  const handleAddContact = (event: BaseSyntheticEvent) => {
+    event.preventDefault();
+
+    const newContact = {
+      name: event.target[0].value,
+      city: event.target[1].value,
+    };
+    setContacts([...contacts, newContact]);
+  };
+
+  return (
+    <div>
+      <div>
+        <h1>Contact book</h1>
+      </div>
+      <div>
+        <h2>Add a new contact</h2>
+        <form onSubmit={handleAddContact}>
+          <label htmlFor="name">Name </label>
+          <input id="name" type="text" name="name" />
+          <br />
+          <br />
+          <label htmlFor="city">City </label>
+          <input id="city" type="text" name="city" />
+          <br />
+          <br />
+          <input type="submit" value="Add contact" />
+        </form>
+      </div>
+      <div>
+        <h2>Contacts</h2>
+        <ul>
+          {contacts.map((contact, index) => (
+            <li key={index}>
+              {contact.name} {contact.city}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default ContactBook;
